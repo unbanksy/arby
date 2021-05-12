72e3f9b33e5f4a4a347fe4572ba01c97f4df1ebc
Dmitri <unbanksyeth@gmail.com>
2021-05-11 22:44:04 -0700

    Compare two trading paths.

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,77 @@
             WOOFY-ETH SLP
           </a>
           to determine an arbitrage opportunity.
+
         </p>
 
+        <p>
+          <a href="https://sushiswap.vision/pair/0x088ee5007c98a9677165d78dd2109ae4a3d04d0c" target="_blank">
+            YFI-ETH SLP
+          </a> price: {{ethToYfiPrice}}
+        </p>
+        <p>
+          <a href="https://sushiswap.vision/pair/0x9ac60b8b33092c2c0b4ca5af0dec2bcb84657e12" target="_blank">
+            WOOFY-ETH SLP
+          </a>
+          price: {{woofyToEthPrice}}
+        </p>
 
-        <ul class="list-group">
-          <li class="list-group-item">
-            <a href="https://sushiswap.vision/pair/0x088ee5007c98a9677165d78dd2109ae4a3d04d0c" target="_blank">
-              YFI-ETH SLP
-            </a> price: {{yfiEthPrice}}
-          </li>
+        <div class="row align-items-center mb-2">
+          <div class="col-sm-6 offset-sm-3">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon3">Trade Size (in ETH)</span>
+              <input type="text" class="form-control" v-model="inputETH"  />
+            </div>
 
-          <li class="list-group-item">
-            <a href="https://sushiswap.vision/pair/0x9ac60b8b33092c2c0b4ca5af0dec2bcb84657e12" target="_blank">
-              WOOFY-ETH SLP
-            </a>
-            price: {{woofyEthPrice}}
-          </li>
+          </div>
+        </div>
 
-          <li class="list-group-item">
-            Assuming a {{inputETH}} ETH purchase, you'll get {{outputETH}} ETH via ETH -> YFI -> WOOFY -> ETH trade.
-          </li>
 
-          <li class="list-group-item  list-group-item-info fw-bold">
-            This translates to an arbitrage opportunity of {{outputETH - inputETH}} ETH.
-          </li>
+        <div class="row">
+          <div class="col-sm-6">
+            <ul class="list-group">
+              <li class="list-group-item fw-bold">
+                ETH -> YFI -> WOOFY -> ETH trade
+              </li>
 
-          <li class="list-group-item">
-            NOTE: This ignores slippage and gas tx costs. That (most probably) erases the arb opportunity.
-          </li>
-        </ul>
+              <li class="list-group-item">
+                You'll get {{tradeYfiToWoofy}} ETH
+              </li>
+
+              <li v-bind:class="['list-group-item', 'fw-bold', tradeYfiToWoofy - inputETH > 0 ? 'list-group-item-success' : 'list-group-item-danger']">
+                Arbitrage opportunity: {{tradeYfiToWoofy - inputETH}} ETH
+              </li>
+
+              <li class="list-group-item">
+                NOTE: This ignores slippage and gas tx costs. That (most probably) erases the arb opportunity.
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-sm-6">
+            <ul class="list-group">
+              <li class="list-group-item fw-bold">
+                ETH -> WOOFY -> YFI -> ETH trade
+              </li>
+
+              <li class="list-group-item">
+                You'll get {{tradeWoofyToYfi}} ETH
+              </li>
+
+
+              <li v-bind:class="['list-group-item', 'fw-bold', tradeWoofyToYfi - inputETH > 0 ? 'list-group-item-success' : 'list-group-item-danger']">
+                Arbitrage opportunity: {{tradeWoofyToYfi - inputETH}} ETH
+              </li>
+
+              <li class="list-group-item">
+                NOTE: This ignores slippage and gas tx costs. That (most probably) erases the arb opportunity.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+
 
       </div>
     </div>
@@ -60,9 +102,8 @@
     data() {
       return {
         inputETH: 1,
-        yfiEthPrice: null,
-        woofyEthPrice: null,
-        outputETH: null
+        tradeWoofyToYfi: null,
+        tradeYfiToWoofy: null,
       };
     },
 
@@ -74,24 +115,29 @@
         // https://etherscan.io/address/0x088ee5007c98a9677165d78dd2109ae4a3d04d0c
         const yfiETH      = new ethers.Contract("0x088ee5007c98a9677165d78dd2109ae4a3d04d0c", YfiETH, provider);
         const reserves    = await yfiETH.getReserves();
-        this.yfiEthPrice  = reserves[0] / reserves[1];
+        this.ethToYfiPrice  = reserves[0] / reserves[1];
 
         // WOOFY-ETH: https://sushiswap.vision/pair/0x9ac60b8b33092c2c0b4ca5af0dec2bcb84657e12
         // https://etherscan.io/address/0x9ac60b8b33092c2c0b4ca5af0dec2bcb84657e12
         const woofyETH     = new ethers.Contract("0x9ac60b8b33092c2c0b4ca5af0dec2bcb84657e12", WoofyETH, provider);
         const wethr        = await woofyETH.getReserves();
-        this.woofyEthPrice = wethr[0] / (wethr[1] * Math.pow(10, 6));
+        this.woofyToEthPrice = wethr[0] / (wethr[1] * Math.pow(10, 6));
 
-        console.log("this.yfiEthPrice = ", this.yfiEthPrice)
-        console.log("this.woofyEthPrice = ", this.woofyEthPrice)
+        this.tradeYfiToWoofy = this.inputETH * this.ethToYfiPrice * Math.pow(10, 6) * this.woofyToEthPrice;
 
-        const yfi    = this.inputETH * this.yfiEthPrice;
-        const woofy  = yfi * Math.pow(10, 6);
-        const newEth = this.woofyEthPrice * woofy;
-        this.outputETH = newEth;
+        this.tradeWoofyToYfi = (this.inputETH * (1 / this.woofyToEthPrice) / Math.pow(10, 6)) * (1 / this.ethToYfiPrice);
+
+
 
       })();
 
+    },
+
+    methods: {
+      setEthSize(evt) {
+        console.log("evt.target.value = ", evt.target.value)
+        this.ethSize = evt.target.value;
+      }
     }
 
 
